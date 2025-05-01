d7fdfc4fc7b0c5b96b79a653878980c8cbf03bba
Petrov Dmitry <d.petrov@infosec.ru>
2025-05-01 18:20:30 +0300

    added changes to TestPracticeForm.java

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -4,9 +4,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestPracticeForm {
 
@@ -15,22 +16,55 @@ public class TestPracticeForm {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.holdBrowserOpen = true;
-//        Configuration.timeout = 5000; // default 4000
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
-    void fillFormTest() {
-        open("/text-box");
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
-        $("#submit").click();
+    void fillPracticeForm() {
+        // Relative path for baseUrl
+        open("/automation-practice-form");
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        // Set basic info
+        $("#firstName").setValue("Dmitry");
+        $("#lastName").setValue("Endo");
+        $("#userEmail").setValue("dmitry@bk.com");
+        $("label[for='gender-radio-1']").shouldBe(visible, enabled).click();
+        $("#userNumber").setValue("9998554545");
+
+        // Choose 'Date of Birth' field by calendar widget
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("0");
+        $(".react-datepicker__year-select").selectOptionByValue("1980");
+        $$("div.react-datepicker__day:not(.react-datepicker__day--outside-month)")
+                .findBy(text("1"))
+                .shouldBe(visible)
+                .click();
+
+        // Fill out 'Subjects' field
+        $("#subjectsInput").setValue("math").pressEnter();
+        $("#subjectsInput").setValue("art").pressEnter();
+        $("#subjectsInput").setValue("eng").pressEnter();
+
+        // Choose checkboxes for 'Hobbies'
+        $("label[for='hobbies-checkbox-1']").shouldBe(visible, enabled).click();
+        $("label[for='hobbies-checkbox-3']").shouldBe(visible, enabled).click();
+
+        // File upload for 'Picture'
+        File imageToUpload = new File("src/test/resources/avatar.jpg");
+        $("#uploadPicture").uploadFile(imageToUpload);
+
+        // Fill out 'Current Address' field
+        $("#currentAddress").setValue("Arbat street");
+
+        // Choose state
+        $("#state").scrollIntoView(true).click();
+        $$("div[id^='react-select'][class*='option']").findBy(text("NCR")).click();
+
+        // Choose city
+        $("#city").scrollIntoView(true).click();
+        $$("div[id^='react-select'][class*='option']").findBy(text("Noida")).click();
+
+        // Click 'Submit' button
+        $("#submit").click();
     }
 }
