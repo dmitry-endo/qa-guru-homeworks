30ace397442853cc8d23ad916c8e0277367d329d
d.petrov <d.petrov@infosec.ru>
2025-05-27 16:58:59 +0300

    Deleted line Configuration.holdBrowserOpen = true;

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -15,7 +15,6 @@ public class TestPracticeForm {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-//        Configuration.holdBrowserOpen = true;
     }
 
     @Test
