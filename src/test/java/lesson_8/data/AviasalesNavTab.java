52bea2272aa03a1d22ed2dc5eb9da927e2bef30c
d.petrov <d.petrov@infosec.ru>
2025-06-30 02:06:18 +0300

    Added Enum AviasalesNavTab.java

---
+++ src/test/java/lesson_8/data/AviasalesNavTab.java
@@ -0,0 +1,16 @@
+package lesson_8.data;
+
+public enum AviasalesNavTab {
+
+    AVIABILETY("Авиабилеты"),
+    OTELI("Отели"),
+    KOROCHE("Короче"),
+    IZBRANNOE("Избранное");
+
+    public final String title;
+
+    AviasalesNavTab(String title) {
+        this.title = title;
+
+    }
+}
