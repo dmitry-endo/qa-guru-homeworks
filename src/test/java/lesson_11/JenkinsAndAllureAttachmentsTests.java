67fe3ed65aa78676659bbd95d5ce29a07649d282
d.petrov <d.petrov@infosec.ru>
2025-07-14 21:33:45 +0300

    Added Allure steps for better reports structure

--- src/test/java/lesson_11/JenkinsAndAllureAttachmentsTests.java
+++ src/test/java/lesson_11/JenkinsAndAllureAttachmentsTests.java
@@ -14,6 +14,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Map;
 
+import static io.qameta.allure.Allure.step;
+
 @DisplayName("Тесты на заполнение формы demoqa - Practice Form")
 @Tag("demoqa_lesson11")
 public class JenkinsAndAllureAttachmentsTests {
@@ -50,33 +52,35 @@ public class JenkinsAndAllureAttachmentsTests {
     @DisplayName("Тест на успешное заполнение формы")
     void successfulPracticeFormTest() {
 
-        practiceFormPage.openPage()
-                .setFirstName(user.getFirstName())
-                .setLastName(user.getLastName())
-                .setEmail(user.getEmail())
-                .setGender(user.getGender())
-                .setUserNumber(user.getUserNumber())
-                .setDateOfBirth(user.getDay(), user.getMonth(), user.getYear())
-                .setSubject(user.getSubject())
-                .setHobby(user.getHobby())
-                .uploadPicture(user.getPicture())
-                .setAddress(user.getCurrentAddress())
-                .setState(user.getState())
-                .setCity(user.getCity())
-                .clickSubmit();
+        step("Заполняем Practice Form и жмем submit", () ->
+                practiceFormPage.openPage()
+                    .setFirstName(user.getFirstName())
+                    .setLastName(user.getLastName())
+                    .setEmail(user.getEmail())
+                    .setGender(user.getGender())
+                    .setUserNumber(user.getUserNumber())
+                    .setDateOfBirth(user.getDay(), user.getMonth(), user.getYear())
+                    .setSubject(user.getSubject())
+                    .setHobby(user.getHobby())
+                    .uploadPicture(user.getPicture())
+                    .setAddress(user.getCurrentAddress())
+                    .setState(user.getState())
+                    .setCity(user.getCity())
+                    .clickSubmit());
 
-        practiceFormPage.checkResultTableHasAppeared()
-                .checkResultTableRecord("Student Name", user.getFullName())
-                .checkResultTableRecord("Student Email", user.getEmail())
-                .checkResultTableRecord("Gender", user.getGender())
-                .checkResultTableRecord("Mobile", user.getUserNumber())
-                .checkResultTableRecord("Date of Birth", user.getFullBirthDate())
-                .checkResultTableRecord("Subjects", user.getSubject())
-                .checkResultTableRecord("Hobbies", user.getHobby())
-                .checkResultTableRecord("Picture", user.getPicture())
-                .checkResultTableRecord("Address", user.getCurrentAddress())
-                .checkResultTableRecord("State and City", user.getStateAndCity())
-                .clickCloseModal();
+        step("Проверяем результаты в модальном окне и закрываем его", () ->
+                practiceFormPage.checkResultTableHasAppeared()
+                    .checkResultTableRecord("Student Name", user.getFullName())
+                    .checkResultTableRecord("Student Email", user.getEmail())
+                    .checkResultTableRecord("Gender", user.getGender())
+                    .checkResultTableRecord("Mobile", user.getUserNumber())
+                    .checkResultTableRecord("Date of Birth", user.getFullBirthDate())
+                    .checkResultTableRecord("Subjects", user.getSubject())
+                    .checkResultTableRecord("Hobbies", user.getHobby())
+                    .checkResultTableRecord("Picture", user.getPicture())
+                    .checkResultTableRecord("Address", user.getCurrentAddress())
+                    .checkResultTableRecord("State and City", user.getStateAndCity())
+                    .clickCloseModal());
     }
 
     @Test
@@ -88,39 +92,43 @@ public class JenkinsAndAllureAttachmentsTests {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM,yyyy", Locale.ENGLISH);
         String formattedDate = today.format(formatter);
 
-        practiceFormPage.openPage()
-                .setFirstName(user.getFirstName())
-                .setLastName(user.getLastName())
-                .setGender(user.getGender())
-                .setUserNumber(user.getUserNumber())
-                .clickSubmit();
+        step("Заполняем обязательные поля Practice Form и жмем submit", () ->
+                practiceFormPage.openPage()
+                    .setFirstName(user.getFirstName())
+                    .setLastName(user.getLastName())
+                    .setGender(user.getGender())
+                    .setUserNumber(user.getUserNumber())
+                    .clickSubmit());
 
-        practiceFormPage.checkResultTableHasAppeared()
-                .checkResultTableRecord("Student Name", user.getFullName())
-                .checkResultTableRecord("Gender", user.getGender())
-                .checkResultTableRecord("Mobile", user.getUserNumber())
-                .checkResultTableRecord("Date of Birth", formattedDate)
-                .checkResultTableRecordIsEmpty("Student Email")
-                .checkResultTableRecordIsEmpty("Subjects")
-                .checkResultTableRecordIsEmpty("Hobbies")
-                .checkResultTableRecordIsEmpty("Picture")
-                .checkResultTableRecordIsEmpty("Address")
-                .checkResultTableRecordIsEmpty("State and City")
-                .clickCloseModal();
+        step("Проверяем результаты в модальном окне и закрываем его", () ->
+                practiceFormPage.checkResultTableHasAppeared()
+                    .checkResultTableRecord("Student Name", user.getFullName())
+                    .checkResultTableRecord("Gender", user.getGender())
+                    .checkResultTableRecord("Mobile", user.getUserNumber())
+                    .checkResultTableRecord("Date of Birth", formattedDate)
+                    .checkResultTableRecordIsEmpty("Student Email")
+                    .checkResultTableRecordIsEmpty("Subjects")
+                    .checkResultTableRecordIsEmpty("Hobbies")
+                    .checkResultTableRecordIsEmpty("Picture")
+                    .checkResultTableRecordIsEmpty("Address")
+                    .checkResultTableRecordIsEmpty("State and City")
+                    .clickCloseModal());
     }
 
     @Test
     @DisplayName("Негативный тест на заполнение формы")
     void negativePracticeFormTest() {
 
-        practiceFormPage.openPage()
-                .setFirstName(user.getFirstName())
-                .setEmail(user.getEmail())
-                .setUserNumber(user.getUserNumber())
-                .uploadPicture(user.getPicture())
-                .clickSubmit();
+        step("Заполняем не все обязательные поля Practice Form и жмем submit", () ->
+                practiceFormPage.openPage()
+                    .setFirstName(user.getFirstName())
+                    .setEmail(user.getEmail())
+                    .setUserNumber(user.getUserNumber())
+                    .uploadPicture(user.getPicture())
+                    .clickSubmit());
 
-        practiceFormPage.checkResultTableHasNotAppeared();
+        step("Проверяем, что модальное окно с результатами не появилось",
+                practiceFormPage::checkResultTableHasNotAppeared);
     }
 
     @Disabled
