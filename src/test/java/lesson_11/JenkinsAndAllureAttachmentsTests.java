b7d6190961cd79a7199734888b69fe60b1e8693f
d.petrov <d.petrov@infosec.ru>
2025-07-16 13:17:37 +0300

    Removed @BeforeAll and @AfterEach that are now in TestBase

--- src/test/java/lesson_11/JenkinsAndAllureAttachmentsTests.java
+++ src/test/java/lesson_11/JenkinsAndAllureAttachmentsTests.java
@@ -1,49 +1,19 @@
 package lesson_11;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import data.PracticeFormUser;
-import helpers.Attachments;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PracticeFormPage;
+import tests.TestBase;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
-import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 
 @DisplayName("Тесты на заполнение формы demoqa - Practice Form")
 @Tag("demoqa_lesson11")
-public class JenkinsAndAllureAttachmentsTests {
-
-    @BeforeAll
-    static void testsSetup() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
-
-        SelenideLogger.addListener("allure", new AllureSelenide());
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attachments.screenshotAs("Final result");
-        Attachments.pageSource();
-        Attachments.browserConsoleLogs();
-        Attachments.addVideo();
-    }
+public class JenkinsAndAllureAttachmentsTests extends TestBase {
 
     private final PracticeFormPage practiceFormPage = new PracticeFormPage();
     private final PracticeFormUser user = new PracticeFormUser();
